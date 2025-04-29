662986f1e1a18f3c3b948d706ef74fd6dc321a4e
gustavoclay <gustavoclay@gmail.com>
2025-04-29 19:53:13 -0300

    Correção da Prova P1

--- 08-navegacao-abas/screens/HomeScreen.jsx
+++ 08-navegacao-abas/screens/HomeScreen.jsx
@@ -1,31 +1,22 @@
-import { StyleSheet, View } from 'react-native'
+import { StyleSheet, View, ScrollView } from 'react-native'
 import { Text, Card, Title, Paragraph } from 'react-native-paper'
 import React from 'react'
 
 
 export default function HomeScreen() {
   return (
-    <View style={styles.container}>
-      <Text variant='headlineLarge' style={{ textAlign: 'center' }}>Tela de Início</Text>
+      <View style={styles.container}>
+        <Text variant='headlineLarge' style={{ textAlign: 'center' }}>Tela de Início</Text>
 
-      <Card style={{ width: '90%' }}>
-        <Card.Content>
-          <Title>Um título</Title>
-          <Paragraph>lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum </Paragraph>
-        </Card.Content>
-        <Card.Cover source={{ uri: 'https://picsum.photos/700'}} />
-      </Card>
+        <Card style={{ width: '90%', height: '90%' }}>
+          <Card.Content>
+            <Title>Um título</Title>
+            <Paragraph>lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum </Paragraph>
+          </Card.Content>
+          <Card.Cover source={{ uri: 'https://picsum.photos/700' }} resizeMode='stretch' style={{height: 400}} />
+        </Card>
+      </View>
 
-      <Card style={{ width: '90%' }}>
-        <Card.Content>
-          <Title>Um título</Title>
-          <Paragraph>lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum </Paragraph>
-        </Card.Content>
-        <Card.Cover source={{ uri: 'https://picsum.photos/700' }} />
-      </Card>
-
-
-    </View>
   )
 }
 
