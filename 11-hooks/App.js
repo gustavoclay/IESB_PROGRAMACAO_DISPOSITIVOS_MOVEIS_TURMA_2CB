90b8fb9645fa20da6d3f11bfafad971f76c54d1a
gustavoclay <gustavoclay@gmail.com>
2025-05-13 20:34:06 -0300

    utilizando estado

---
+++ 11-hooks/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, View } from 'react-native';
+
+import NomeNumero from './components/NomeNumero';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <StatusBar style="auto" />
+      
+      <NomeNumero />
+
+
+
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
