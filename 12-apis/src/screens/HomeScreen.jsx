0986f5fce2a5fa92171639b61b9bc03c363b75eb
gustavoclay <gustavoclay@gmail.com>
2025-05-20 21:48:05 -0300

    Iniciando consumo de api

--- 12-apis/src/screens/HomeScreen.jsx
+++ 12-apis/src/screens/HomeScreen.jsx
@@ -1,9 +1,11 @@
-import { StyleSheet, View } from 'react-native'
-import { Text } from 'react-native-paper'
-import React, { useEffect } from 'react'
 import axios from 'axios'
+import React, { useEffect, useState } from 'react'
+import { FlatList, StyleSheet, View } from 'react-native'
+import { Avatar, Card, IconButton } from 'react-native-paper'
 
-export default function HomeScreen() {
+export default function HomeScreen({ navigation, route }) {
+
+  const [usuarios, setUsuarios] = useState([])
 
   // Fazer algo quando o usuário entrar na tela (Componente for montado)
   useEffect(() => {
@@ -12,18 +14,35 @@ export default function HomeScreen() {
     // Fazer uma requisição para buscar a lista de usuários
     axios.get('https://dummyjson.com/users')
       .then(resposta => {
-          console.log(resposta.data.users)
+        console.log(resposta.data.users)
+        setUsuarios(resposta.data.users)
       })
       .catch(erro => {
         console.log(erro)
       })
+
   }, [])
 
   return (
     <View style={styles.container}>
-      
 
-
+      <FlatList
+        style={{ marginBottom: 45 }}
+        data={usuarios}
+        renderItem={({ item }) => (
+          <Card
+            style={{ margin: 8 }}
+            onPress={() => navigation.navigate('UsuarioScreen', item.id)}
+          >
+            <Card.Title
+              title={item.firstName + " " + item.lastName}
+              subtitle={item.email}
+              left={(props) => <Avatar.Image {...props} source={{ uri: item.image }} />}
+              right={() => <IconButton icon='chevron-right' size={30} />}
+            />
+          </Card>
+        )}
+      />
 
     </View>
   )
@@ -33,7 +52,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
   },
 })
--- 12-apis/src/screens/UsuarioScreen.jsx
+++ 12-apis/src/screens/UsuarioScreen.jsx
@@ -1,7 +1,10 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 
-export default function UsuarioScreen() {
+export default function UsuarioScreen({ navigation, route }) {
+
+  console.log("ID DO USUARIO RECEBIDO: ", route.params)
+
   return (
     <View>
       <Text>UsuarioScreen</Text>
